c852bbfc2e1f72eb206910c5aa5200c89100cb82
dariamozgunova <savemyselfkm@gmail.com>
2025-09-22 14:06:07 +0300

    Реализован функционал окна добавления товара в магазине.

--- AddProductWindow.xaml.cs
+++ AddProductWindow.xaml.cs
@@ -14,14 +14,52 @@ using System.Windows.Shapes;
 
 namespace ISIP422_Mozgunova
 {
-    /// <summary>
-    /// Логика взаимодействия для AddProductWindow.xaml
-    /// </summary>
     public partial class AddProductWindow : Window
     {
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductQuantity { get; set; }
+        public string ProductCategory { get; set; }
+
         public AddProductWindow()
         {
             InitializeComponent();
+            CategoryComboBox.SelectedIndex = 0;
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(NameTextBox.Text))
+            {
+                MessageBox.Show("Введите название товара");
+                return;
+            }
+
+            if (!decimal.TryParse(PriceTextBox.Text, out decimal price) || price <= 0)
+            {
+                MessageBox.Show("Введите цену");
+                return;
+            }
+
+            if (!int.TryParse(QuantityTextBox.Text, out int quantity) || quantity < 0)
+            {
+                MessageBox.Show("Введите количество");
+                return;
+            }
+
+            ProductName = NameTextBox.Text;
+            ProductPrice = price;
+            ProductQuantity = quantity;
+            ProductCategory = (CategoryComboBox.SelectedItem as ComboBoxItem).Content.ToString();
+
+            DialogResult = true;
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
         }
     }
 }
