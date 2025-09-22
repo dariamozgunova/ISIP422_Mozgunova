634ac67604b91081cb4b1967804835624cac5f52
dariamozgunova <savemyselfkm@gmail.com>
2025-09-22 13:47:54 +0300

    Создан класс продуктов.

---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP422_Mozgunova
+{
+    public class Product
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public bool IsAvailable { get; set; }
+        public string Category { get; set; }
+    }
+}
