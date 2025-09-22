9cc85456d21f6f065fa3a3b3222ca1d1839b2ec5
dariamozgunova <savemyselfkm@gmail.com>
2025-09-22 14:51:32 +0300

    Создан класс товаров.

--- Classes/Product.cs
+++ Classes/Product.cs
@@ -13,6 +13,6 @@ namespace ISIP422_Mozgunova
         public decimal Price { get; set; }
         public int Quantity { get; set; }
         public bool IsAvailable { get; set; }
-        public string Category { get; set; }
+        public string Categor { get; set; }
     }
 }
